54a140871708e5127657957d791fc0399ee73555
AlgorythmiK <2handheld@gmail.com>
2022-01-18 16:03:24 +0100

    updated MemberListViewApapter.kt . setting up di

---
+++ app/src/main/java/joseph/assessment/assignment/presenter/fragment/members/MemberListViewApapter.kt
@@ -0,0 +1,47 @@
+package joseph.assessment.assignment.presenter.fragment.members
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import joseph.assessment.assignment.data.member.Member
+import joseph.assessment.assignment.databinding.MemberViewitemLayoutBinding
+
+class MemberListViewApapter : RecyclerView.Adapter<MemberListViewApapter.MemberItemViewHolder>() {
+
+
+    private val list: MutableList<Member> = mutableListOf()
+
+    public fun setList(list: MutableList<Member>) {
+        this.list.clear()
+        this.list.addAll(list)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MemberItemViewHolder =
+        MemberItemViewHolder(
+            MemberViewitemLayoutBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+
+    override fun onBindViewHolder(holder: MemberItemViewHolder, position: Int) {
+        holder.bind(list[position])
+    }
+
+
+    override fun getItemCount(): Int = list.size
+
+
+    class MemberItemViewHolder(val binding: MemberViewitemLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+            fun bind(member:Member){
+                binding.apply {
+
+                }
+            }
+
+    }
+
+}
