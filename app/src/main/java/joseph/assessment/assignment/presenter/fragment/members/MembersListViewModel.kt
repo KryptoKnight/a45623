be5c88ae3e587a4569c84583e5fbaea947d5af52
AlgorythmiK <2handheld@gmail.com>
2022-01-18 15:45:29 +0100

    setup co-routines

--- app/src/main/java/joseph/assessment/assignment/presenter/fragment/members/MembersListViewModel.kt
+++ app/src/main/java/joseph/assessment/assignment/presenter/fragment/members/MembersListViewModel.kt
@@ -1,17 +1,29 @@
 package joseph.assessment.assignment.presenter.fragment.members
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import joseph.assessment.assignment.data.member.Member
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MembersListViewModel : ViewModel() {
     // TODO: Implement the ViewModel
 
+    private val exceptionHandler = CoroutineExceptionHandler { _, exception ->
+        //Timber.e(exception)
 
+    }
 
 
 
     public fun fetchMembers(){
+        viewModelScope.launch(exceptionHandler) {
+            withContext(Dispatchers.IO) {
 
+            }
+        }
     }
 
 
