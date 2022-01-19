17e4563d9687bd71d9b75c0f9798e154d57fe49d
AlgorythmiK <2handheld@gmail.com>
2022-01-19 11:20:06 +0100

    implemented LikeServiceImpl.kt

--- app/src/main/java/joseph/assessment/assignment/data/like/LikeService.kt
+++ app/src/main/java/joseph/assessment/assignment/data/like/LikeService.kt
@@ -3,5 +3,5 @@ package joseph.assessment.assignment.data.like
 import joseph.assessment.assignment.data.like.model.Like
 
 interface LikeService {
-    fun updateLike(like:Like):Boolean
+    fun updateLike(like:Like)
 }
--- app/src/main/java/joseph/assessment/assignment/domain/services/LikeServiceImpl.kt
+++ app/src/main/java/joseph/assessment/assignment/domain/services/LikeServiceImpl.kt
@@ -1,13 +1,22 @@
 package joseph.assessment.assignment.domain.services
 
+import joseph.assessment.assignment.data.db.LikeEntity
 import joseph.assessment.assignment.data.like.LikeService
 import joseph.assessment.assignment.data.like.model.Like
 import joseph.assessment.assignment.domain.db.dao.LikeDAO
 
 class LikeServiceImpl(val likeDAO: LikeDAO) : LikeService {
 
-    override fun updateLike(like:Like): Boolean {
-        TODO("Not yet implemented")
+    override fun updateLike(like:Like) {
+        likeDAO.updateLike(LikeEntity(like.memberId,like.update,like.id))
+    }
+
+    fun fromLikeEntity(likeEntityList:List<LikeEntity>):List<Like>{
+        return likeEntityList.map { Like(it.memberId,it.update,it.id) }
+    }
+
+    fun fromLike(likeList:List<Like>):List<LikeEntity>{
+        return likeList.map { LikeEntity(it.memberId,it.update,it.id) }
     }
 
 }
